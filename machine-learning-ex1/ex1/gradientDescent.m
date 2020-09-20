95789fc491f4112a946ccd27af4fe37cc288b266
Abhijith Anandakrishnan <abhijithananthan@gmail.com>
2020-09-20 23:22:25 +0530

    Completed third task in first assignment

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,8 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
+    h = X*theta;
+    delta = (1/m)* ones(1, m)*((h .- y) .* X);
+    theta = theta - alpha*delta';
 
 
 
